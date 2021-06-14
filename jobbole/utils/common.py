2c587a16e31f877262986585a3b132727a518040
robertzhangwenjie <robertzhangwenjie@gmail.com>
2021-06-14 12:35:43 +0800

    feat(selenium): add common methods for judging element is exists or dispaly

--- jobbole/utils/common.py
+++ jobbole/utils/common.py
@@ -3,6 +3,8 @@ import hashlib
 import typing
 
 from itemloaders.processors import Join
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver import ActionChains
 
 
 def get_md5(value: typing.Union[str,bytes]):
@@ -18,6 +20,44 @@ def get_md5(value: typing.Union[str,bytes]):
 
 def date_from_datetimestr(value):
     return datetime.datetime.strptime(value,"%Y-%m-%d %H:%M:%S").date()
+
+
+# 判断元素是否存在,存在就返回该元素
+def is_element_exist(driver,element):
+    try:
+        ret = driver.find_element(*element)
+    except (NoSuchElementException) as err:
+        print(err)
+        return False
+    if ret:
+        return ret
+    return False
+
+# 判断元素是否可见
+def is_element_display(driver,element):
+    try:
+        ret = driver.find_element(*element)
+    except (NoSuchElementException) as err:
+        print(err)
+        return False
+    if ret.is_displayed():
+        return True
+    return False
+
+
+def scroll_btn(driver,btn_selector,offset):
+    '''
+    拖动滑块验证
+    :param driver:
+    :param offset: 拖动的距离
+    :return:
+    '''
+    button = driver.find_element(*btn_selector)
+    action = ActionChains(driver)
+    action.click_and_hold(button).perform()
+    action.reset_actions()
+    action.move_by_offset(offset, 0).perform()
+
 
 def join(value):
     res = Join(",")(value)
